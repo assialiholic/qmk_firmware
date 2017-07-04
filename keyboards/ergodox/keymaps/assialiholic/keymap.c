8402b549c582bfb4c81cd387f6e51649d3cbbad0
Atsushi Handa <a.handa@24-7.co.jp>
2017-07-04 20:38:04 +0900

    no message

--- keyboards/ergodox/keymaps/assialiholic/keymap.c
+++ keyboards/ergodox/keymaps/assialiholic/keymap.c
@@ -39,24 +39,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,            KC_1,    KC_2,          KC_3,    KC_4,   KC_5,   KC_LEFT,
+        KC_ESC,            KC_1,    KC_2,          KC_3,    KC_4,   KC_5,   KC_QUOT,
         KC_TAB,            KC_Q,    KC_W,          KC_E,    KC_R,   KC_T,   TG(SYMB),
         LT(SYMB, KC_CAPS), KC_A,    KC_S,          KC_D,    KC_F,   KC_G,
         KC_LSFT,           KC_Z,    KC_X,          KC_C,    KC_V,   KC_B,   KC_HENK,
-        GUI_T(KC_QUOT),    KC_LALT, CTL_T(KC_NO),  KC_LEFT, KC_RGHT,
+        GUI_T(KC_NO),    KC_LALT, CTL_T(KC_NO),  KC_LEFT, KC_RGHT,
 
-                                              ALT_T(KC_APP),  KC_FN19,
+                                              KC_PLUS,  KC_LBRC,
                                                               KC_HOME,
                                              KC_MHEN, CTL_T(KC_SPC), KC_HENK,
 
         // right hand
-             KC_RGHT,      KC_6,   KC_7,  KC_8,    KC_9,    KC_0,              KC_BSPC,
+             KC_BSLASH,      KC_6,   KC_7,  KC_8,    KC_9,    KC_0,              KC_BSPC,
              TG(SYMB),     KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,              KC_MINS,
                            KC_H,   KC_J,  KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
              KC_ENT,       KC_N,   KC_M,  KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
                                    KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,           GUI_T(KC_KP_PLUS),
 
-             KC_FN19, CTL_T(KC_ESC),
+             KC_RBRC, KC_EQL,
              KC_PGUP,
              KC_HENK, CTL_T(KC_SPC), KC_MHEN
     ),
