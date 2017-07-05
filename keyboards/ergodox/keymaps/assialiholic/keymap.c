5e03e62f48eb87f98b91cb73d126b8556557c714
Atsushi Handa <a.handa@24-7.co.jp>
2017-07-05 11:55:24 +0900

    add MEDIA layer

--- keyboards/ergodox/keymaps/assialiholic/keymap.c
+++ keyboards/ergodox/keymaps/assialiholic/keymap.c
@@ -7,6 +7,10 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
+
+#define A_S_T(kc) MT((MOD_LALT | MOD_LSFT), kc)
+
+
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
@@ -51,10 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // right hand
              KC_BSLASH, KC_6, KC_7,  KC_8,    KC_9,    KC_0,              KC_BSPC,
-             TG(SYMB),  KC_Y, KC_U,  KC_I,    KC_O,    KC_P,              KC_MINS,
+             TG(MDIA),  KC_Y, KC_U,  KC_I,    KC_O,    KC_P,              KC_MINS,
                         KC_H, KC_J,  KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
              KC_ENT,    KC_N, KC_M,  KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
-                              KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,           GUI_T(KC_KP_PLUS),
+                              KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,           A_S_T(KC_PLUS),
 
              KC_RBRC, KC_EQL,
              KC_PGUP,
@@ -85,25 +89,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
         KC_ESC,            KC_F1,   KC_F2, KC_F3,  KC_F4, KC_F5,   KC_F6,
-        KC_TAB,            KC_Q,    KC_7,  KC_8,   KC_9,  KC_MINS, TG(SYMB),
+        KC_TAB,            KC_Q,    KC_7,  KC_8,   KC_9,  KC_MINS, TG(KC_TRNS),
         LT(SYMB, KC_CAPS), KC_DOWN, KC_4,  KC_5,   KC_6,  KC_PLUS,
         KC_LSFT,           KC_AMPR, KC_1,  KC_2,   KC_3,  KC_SLSH, KC_HENK,
         GUI_T(KC_NO),      KC_LALT, KC_0,  KC_DOT, KC_ASTR,
 
-                                     ALT_T(KC_APP),  KC_FN19,
-                                                     KC_HOME,
-                             KC_MHEN, CTL_T(KC_SPC), KC_HENK,
+                                      KC_TRNS, KC_TRNS,
+                                               KC_TRNS,
+                             KC_TRNS, KC_TRNS, KC_TRNS,
                             
        // right hand
-       KC_F7,    KC_F8, KC_F9,    KC_F10,   KC_F11,    KC_F12,    KC_BSPC,
-       TG(SYMB), KC_Y,  KC_EQL,   KC_MINUS, KC_GRAVE,  KC_BSLASH, KC_DELETE,
-                 KC_H,  KC_J,     KC_CIRC,  KC_AT,     KC_LBRC,   KC_ENT,
-       KC_ENT,   KC_N,  KC_M,     KC_COMM,  KC_SCOLON, KC_RBRC,   KC_RSFT,
-                        KC_COMMA, KC_DOT,   KC_SLASH,  KC_RBRC,   KC_PLUS,
+       KC_F7,       KC_F8, KC_F9,    KC_F10,   KC_F11,    KC_F12,    KC_BSPC,
+       TG(KC_TRNS), KC_Y,  KC_EQL,   KC_MINUS, KC_GRAVE,  KC_BSLASH, KC_DELETE,
+                    KC_H,  KC_J,     KC_CIRC,  KC_AT,     KC_LBRC,   KC_ENT,
+       KC_ENT,      KC_N,  KC_M,     KC_COMM,  KC_SCOLON, KC_RBRC,   KC_RSFT,
+                           KC_COMMA, KC_DOT,   KC_SLASH,  KC_RBRC,   KC_PLUS,
 
-       KC_FN19, CTL_T(KC_ESC),
-       KC_PGUP,
-       KC_HENK, CTL_T(KC_SPC), KC_MHEN
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -129,19 +133,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 [MDIA] = KEYMAP(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_MS_WH_UP, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_MS_BTN3,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_DOWN, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+                                                                       KC_TRNS, KC_TRNS,
+                                                                                KC_TRNS,
+                                                              KC_WBAK, KC_TRNS, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_MPRV,  KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,
+       TG(MDIA), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_WBAK
