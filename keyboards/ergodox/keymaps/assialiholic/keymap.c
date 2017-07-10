7710d20be7e9ff9898ec5202c9c9a7db7f68367e
Atsushi Handa <a.handa@24-7.co.jp>
2017-07-10 16:37:40 +0900

    no message

--- keyboards/ergodox/keymaps/assialiholic/keymap.c
+++ keyboards/ergodox/keymaps/assialiholic/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                                   KC_PLUS,  KC_LBRC,
                                                                             KC_HOME,
-                                                    KC_MHEN, CTL_T(KC_SPC), KC_HENK,
+                                                           KC_MHEN, KC_SPC, KC_HENK,
 
         // right hand
              KC_BSLASH, KC_6, KC_7,  KC_8,    KC_9,    KC_0,              KC_BSPC,
