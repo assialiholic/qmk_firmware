2ba842b3de2904cf9d04af79334f9edb78e64c59
Atsushi Handa <a.handa@24-7.co.jp>
2017-07-23 02:10:46 +0900

    tenkey

--- keyboards/ergodox/keymaps/assialiholic/keymap.c
+++ keyboards/ergodox/keymaps/assialiholic/keymap.c
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = KEYMAP(
        // left hand
-        KC_ESC,            KC_F1,   KC_F2, KC_F3,  KC_F4, KC_F5,   KC_F6,
-        KC_TAB,            KC_Q,    KC_7,  KC_8,   KC_9,  KC_MINS, KC_TRNS,
-        LT(SYMB, KC_CAPS), KC_DOWN, KC_4,  KC_5,   KC_6,  KC_PLUS,
-        KC_LSFT,           KC_AMPR, KC_1,  KC_2,   KC_3,  KC_SLSH, KC_HENK,
-        GUI_T(KC_NO),      KC_LALT, KC_0,  KC_DOT, KC_ASTR,
-                                                          KC_TRNS, TG(KC_TRNS),
-                                                                   KC_TRNS,
-                                                 KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_F1,   KC_F2,  KC_F3,   KC_F4,         KC_F5,   KC_F6,
+        KC_TRNS, KC_TRNS, KC_TAB, KC_SLSH, LSFT(JA_CLON), KC_MINS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,          LSFT(KC_SCLN),
+        KC_TRNS, KC_TRNS, KC_4,   KC_5,    KC_6,          KC_ENT,  KC_HENK,
+        KC_TRNS, KC_0,    KC_1,   KC_2,    KC_3,
+                                                KC_TRNS, TG(KC_TRNS),
+                                                             KC_TRNS,
+                                           KC_TRNS, KC_TRNS, KC_TRNS,
 
        // right hand
        KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_DELETE,
