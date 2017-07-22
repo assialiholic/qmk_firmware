7ae378f617bb8b0d22feb6c8b839c422dc6de674
Atsushi Handa <a.handa@24-7.co.jp>
2017-07-22 12:41:37 +0900

    adjust indent

--- keyboards/ergodox/keymaps/assialiholic/keymap.c
+++ keyboards/ergodox/keymaps/assialiholic/keymap.c
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_BSLASH, KC_6, KC_7,  KC_8,    KC_9,    KC_0,              KC_BSPC,
         TG(MDIA),  KC_Y, KC_U,  KC_I,    KC_O,    KC_P,              KC_MINS,
-                  KC_H, KC_J,  KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
+                   KC_H, KC_J,  KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
         KC_ENT,    KC_N, KC_M,  KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
-                        KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,           A_S_T(KC_PLUS),
+                         KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,           A_S_T(KC_PLUS),
 
         KC_RBRC, KC_EQL,
         KC_NO,
